1fbefddbb60c6b025a1eda21a1d3847268da423a
hemanshshridhar <121416974+hemanshshridhar@users.noreply.github.com>
2025-05-07 08:36:40 +0530

    Update agent.py
    - Updated the process method to include the dictionary names.

--- llmadapter/analyzer/agent.py
+++ llmadapter/analyzer/agent.py
@@ -5,43 +5,42 @@ from langchain.prompts.chat import (
 )
 from langchain.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from llmadapter.constants import LLMConstants
-from .template import SYSTEM_PROMPT, USER_PROMPT
+
 import json
 
 
-class AdapterAgent:
+class AnalyzerAgent:
     def __init__(self):
         self.llm = ChatOpenAI(
-            model=LLMConstants.GPT_41_MINI_MODEL,
-            temperature=LLMConstants.TEMPERATURE,
-            max_tokens=LLMConstants.MAX_TOKENS,
-            max_retries=LLMConstants.MAX_RETRIES,
+            model= "gpt-4o",
+            temperature=0,
+            max_tokens= 4000,
+            max_retries= 10,
             streaming=False,
-            tags=LLMConstants.TAGS
+            tags= ["final_response_generator"]
         )
         self.prompt_template = ChatPromptTemplate.from_messages(
             [
                 SystemTemplate(
                     prompt=PromptTemplate(input_variables=[], template=SYSTEM_PROMPT)
                 ),
-                HumanMessagePromptTemplate(
+                 HumanTemplate(
                     prompt=PromptTemplate(
-                        input_variables=["input_dict_1", "input_dict_2"],
+                        input_variables=["data_dict_fixed", "country_dict", "sheetnames"],
                         template=USER_PROMPT
                     )
                 ),
             ]
         )
 
-    def process(self, input_dict_1, input_dict_2):
+    def process(self, input_dict_1, input_dict_2,sheetnames):
         # Convert dicts to strings for prompt formatting
         input_dict_1_str = json.dumps(input_dict_1, indent=2)
         input_dict_2_str = json.dumps(input_dict_2, indent=2)
-
         messages = self.prompt_template.format_messages(
-            input_dict_1=input_dict_1_str,
-            input_dict_2=input_dict_2_str
+            data_dict_fixed=input_dict_1_str,
+            country_dict=input_dict_2_str,
+            sheetnames = sheetnames
         )
 
         response = self.llm(messages).content
